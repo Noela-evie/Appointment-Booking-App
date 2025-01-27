92264b1d9c4660b9c576e116d36d7f19d2d07c8d
Noela-evie <noelanalugwa@gmail.com>
2025-01-27 23:58:20 +0300

    Fix create account issue

--- frontend/src/examples/Navbars/DefaultNavbar/index.js
+++ frontend/src/examples/Navbars/DefaultNavbar/index.js
@@ -103,22 +103,15 @@ function DefaultNavbar({ transparent, light, action }) {
         {authContext.isAuthenticated && (
           <MDBox color="inherit" display={{ xs: "none", lg: "flex" }} m={0} p={0}>
             <DefaultNavbarLink
-              icon="donut_large"
-              name="dashboard"
-              route="/dashboard"
-              light={light}
-            />
-            <DefaultNavbarLink icon="person" name="profile" route="/profile" light={light} />
-            <DefaultNavbarLink
               icon="account_circle"
-              name="sign up"
-              route="/authentication/sign-up"
+              name="Register"
+              route="/auth/register"
               light={light}
             />
             <DefaultNavbarLink
               icon="key"
-              name="sign in"
-              route="/authentication/sign-in"
+              name="Login"
+              route="/auth/login"
               light={light}
             />
           </MDBox>
